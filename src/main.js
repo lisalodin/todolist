f43f995c9e0055291dea34ebb7ae6f128c75a424
Lisa Lodin <lisalodin@hotmail.com>
2025-10-27 13:56:46 +0100

    changed the list to an ul and made list finished true

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import "./style.css";
 
-class List {
+class List { // skapar en klass som heter List
     toDo;
     finished;
 
@@ -10,14 +10,21 @@ class List {
     }
 }
 
-const p1 = new List("Handla", false);
+const p1 = new List("Handla", false); // skapar nya objekt av klassen List
 const p2 = new List("Tvätta", false);
 const p3 = new List("Maila", false);
 const lists = [p1, p2, p3];
 
-    const app = document.getElementById("app"); // hämta elementet med id app
+    const app = document.getElementById("app"); // hämtar elementet med id app
     lists.forEach(list => { // för varje sak i listan lists
-        const listDiv = document.createElement("div"); // skapa en div för varje sak
-        listDiv.innerHTML = `<input type="checkbox"> ${list.toDo}`;
-        app.appendChild(listDiv);
+    const listLi = document.createElement("li"); // skapar en li för varje sak
+    listLi.innerHTML = `<input type="checkbox"> ${list.toDo}`; // lägger till en checkbox och texten från toDo
+
+    listLi.addEventListener("click", () => { // när man klickar på li
+        list.finished = true; // sätter finished till true
+        listLi.remove(); // tas li bort
     });
+
+    app.appendChild(listLi); // lägger till lian i app
+
+});
