09c2234f47dcd95eb39624341e39ad8cc7083821
Lisa Lodin <lisalodin@hotmail.com>
2025-10-27 13:16:44 +0100

    initiated todolist with checkboxes

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
 import "./style.css";
+
+class List {
+    toDo;
+    finished;
+
+    constructor(toDo, finished) {
+        this.toDo = toDo;
+        this.finished = finished;
+    }
+}
+
+const p1 = new List("Handla", false);
+const p2 = new List("Tvätta", false);
+const p3 = new List("Maila", false);
+const lists = [p1, p2, p3];
+
+    const app = document.getElementById("app"); // hämta elementet med id app
+    lists.forEach(list => { // för varje sak i listan lists
+        const listDiv = document.createElement("div"); // skapa en div för varje sak
+        listDiv.innerHTML = `<input type="checkbox"> ${list.toDo}`;
+        app.appendChild(listDiv);
+    });
