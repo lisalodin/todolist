7460707c61f09633eb85079570e090b9678e61a6
Lisa Lodin <lisalodin@hotmail.com>
2025-10-27 15:31:27 +0100

    added localstorage and eventlistner

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import "./style.css";
+// import "./style.css";
 
 class List { // skapar en klass som heter List
     toDo;
@@ -15,14 +15,31 @@ const p2 = new List("Tvätta", false);
 const p3 = new List("Maila", false);
 const lists = [p1, p2, p3];
 
-    const app = document.getElementById("app"); // hämtar elementet med id app
-    lists.forEach(list => { // för varje sak i listan lists
+localStorage.setItem("todos", JSON.stringify(lists)); // Spara objektet i localStorage som en sträng
+
+
+const app = document.getElementById("app"); // hämtar elementet med id app
+app.classList.add("list-group"); // lägger till bootstrap klass för styling
+
+let newTodo = [];
+
+const todosFromLS = localStorage.getItem("todos");
+if (todosFromLS === null) {
+    newTodo = lists; 
+} else {
+    newTodo = JSON.parse(todosFromLS);
+}
+
+
+    newTodo.forEach(list => { // för varje sak i listan lists
     const listLi = document.createElement("li"); // skapar en li för varje sak
+    listLi.classList.add("list-group-item"); // lägger till bootstrap klass för styling
     listLi.innerHTML = `<input type="checkbox"> ${list.toDo}`; // lägger till en checkbox och texten från toDo
 
     listLi.addEventListener("click", () => { // när man klickar på li
         list.finished = true; // sätter finished till true
         listLi.remove(); // tas li bort
+        localStorage.setItem("todos", JSON.stringify(newTodo)); // uppdaterar localStorage
     });
 
     app.appendChild(listLi); // lägger till lian i app
