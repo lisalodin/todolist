d5f92321ea046c87e844d271998e36c585bd0785
Lisa Lodin <lisalodin@hotmail.com>
2025-10-28 23:02:42 +0100

    adding a form

--- src/main.js
+++ src/main.js
@@ -34,22 +34,19 @@ if (todosFromLS === null) { // om det inte finns något i localStorage
 
 
 
-    newTodo.forEach(list => { // för varje sak i listan lists
+const createToDoElement = (list) => {  // funktion för att skapa en todo element
     const listLi = document.createElement("li"); // skapar en li för varje sak
     listLi.classList.add("list-group-item"); // lägger till bootstrap klass för styling
 
     const checkbox = document.createElement("input"); // skapar en checkbox
-    checkbox.type = "checkbox";
-    checkbox.checked = list.finished;
-    listLi.appendChild(checkbox);
+    checkbox.type = "checkbox"; // sätter typen till checkbox
+    checkbox.checked = list.finished; // sätter checkboxen till checked om listan är klar
+    listLi.appendChild(checkbox); // lägger till checkboxen i li
     listLi.appendChild(document.createTextNode(list.toDo)); // lägger till texten i li
 
     if (list.finished === true) { // om listan är klar
         listLi.classList.add("text-decoration-line-through"); // lägger till genomstrykning
-    }   
-
-
-
+    }
 
     checkbox.addEventListener("click", () => { // när man klickar på checkbox
         listLi.classList.toggle("text-decoration-line-through"); // lägger till eller tar bort genomstrykning
@@ -58,5 +55,24 @@ if (todosFromLS === null) { // om det inte finns något i localStorage
     });
 
     app.appendChild(listLi); // lägger till li i app
+}
 
+newTodo.forEach(createToDoElement); // anropar funktionen createToDoElement med varje sak i listan
+
+
+
+
+
+const form = document.getElementById("form"); // hämtar formuläret med id:t form
+form.addEventListener("submit", (event) => { // när man skickar in formuläret
+    event.preventDefault(); // förhindrar att sidan laddas om
+
+const textFromUser = document.getElementById("userInput").value; // hämtar värdet från inputfältet
+
+const added = new List(textFromUser, false); // skapar ett nytt objekt av klassen List med värdet från inputfältet
+
+newTodo.push(added); // lägger till det nya objektet i arrayen newTodo
+localStorage.setItem("todos", JSON.stringify(newTodo)); // uppdaterar localStorage med det nya objektet
+
+createToDoElement(added); // anropar funktionen createToDoElement med det nya objektet
 });
